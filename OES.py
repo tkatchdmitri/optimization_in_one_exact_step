b32e9a5732333a732469339e82b60ef21e944c7b
Dmitri Tkatch <tkatchdmitri@gmail.com>
2025-03-19 08:53:33 +0700

    Update OES.py

--- OES.py
+++ OES.py
@@ -1,24 +1,16 @@
-
-
-def mult(X,Y): # MATRIX MULTIPLICATION
-    return [[X[0][0]*Y[0][0]+X[0][1]*Y[1][0], X[0][0]*Y[0][0]+X[0][1]*Y[1][0]],[X[0][0]*Y[0][0]+X[0][1]*Y[1][0],X[0][0]*Y[0][0]+X[0][1]*Y[1][0]]]
-
-def sub(R,G): # MATRIX SUBTRACTION
-    # print(ROMAN)
-    return [[R[0][0]-G[0][0],R[1][0]-G[1][0]],[R[0][1]-G[0][1],R[1][1]-G[1][1]]]
-
-
-def add(X,Y): # MATRIX ADDITION
-    return [[X[0][0]+Y[0][0],X[1][0]+Y[1][0]],[X[0][1]+Y[0][1],X[1][1]+Y[1][1]]]
-
-def over(X,Y):
-    return [[X[0][0]/Y[0][0],X[1][0]/Y[1][0]],[X[0][1]/Y[0][1],X[1][1]/Y[1][1]]]
-
-A,B,C,D,Z=[[1.0,1.0],[0.0,0.0]],[[0.1,0.1],[0.1,0.1]],[[0.1,0.1],[0.1,0.1]],[[0.0,0.0],[0.0,0.0]],[[0.001,0.001],[0.001,0.001]] # ESTABLISH MATRIX
-for U in range(1): # ONE EXACT STEP
-    # print("U",U)
-    D=mult(mult(A,B),C) # FORWARD
-    for S in range(1000000): # ONE MILLION DERIVATIVES
-        # print("S",S)
-        DD=over(sub(mult(mult(add(A,Z),add(B,Z)),C),mult(mult(sub(A,Z),sub(B,Z)),C)),add(Z,Z)) # PARTIAL
-        B,C=B+DD,C+DD # APPLY PARTIAL
+import numpy as np
+X = np.array([ [0,0,1],[0,1,1],[1,0,1],[1,1,1] ])
+Y = np.array([[0,1,1,0]]).T
+ALPHA,HIDDEN_dim = (0.5,4)
+sin_0 = 2*np.random.random((3,HIDDEN_dim)) - 1
+sin_1 = 2*np.random.random((HIDDEN_dim,1)) - 1
+for a in range(1):
+    level_0 = 1/(1+np.exp(-(np.dot(X,sin_0))))
+    level_1 = 1/(1+np.exp(-(np.dot(level_0,sin_1))))
+    for b in range(60000):
+        level_1_delta = (level_1 - Y)*(level_1*(1-level_1))
+        level_0_delta = level_1_delta.dot(sin_1.T) * (level_0 * (1-level_0))
+    sin_1 -= (ALPHA * level_0.T.dot(level_1_delta))
+    sin_0 -= (ALPHA * X.T.dot(level_0_delta))
+    
+print(level_1)
